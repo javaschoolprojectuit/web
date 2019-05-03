df7644804564754b88b89997d3a432d476660182
Nguyễn Lê Hoài Ân <38518953+hoaian0505@users.noreply.github.com>
2019-05-03 16:27:46 +0700

    Add files via upload

--- Database/BTPM.sql
+++ Database/BTPM.sql
@@ -53,28 +53,29 @@ INSERT INTO USER_INFO(USER_ID, USER_NAME,USER_PASS,USER_EMAIL,IS_ADMIN) VALUES (
 
 INSERT INTO CATEGORIES(CAT_NAME,CAT_IMG,CAT_CONTENT) VALUES ('listen & watch','Banner/watch_listen.jpg','Do you like listening to songs and watching stories and videos in English? In this section you can learn to sing songs in English and watch fun stories and videos.' );
 INSERT INTO CATEGORIES(CAT_NAME,CAT_IMG,CAT_CONTENT) VALUES ('read & write','Banner/read_write.png', 'Do you want to practise your reading and writing in English? In this section you can read and write about interesting topics.');
-INSERT INTO CATEGORIES(CAT_NAME,CAT_IMG,CAT_CONTENT) VALUES ('gramma & vocabulary','Banner/gramma_vocabulary.jpg', 'Do you want to practise your English grammar and learn new words? In this section you can learn about grammar rules, play word games and watch fun videos. Watch the grammar videos, play the grammar games and print the grammar worksheets.');
 INSERT INTO CATEGORIES(CAT_NAME,CAT_IMG,CAT_CONTENT) VALUES ('speak & spell','Banner/speak_spell.jpg','Do you want to improve your spelling and pronunciation in English? In this section you can learn how to say and spell English words with Sam and Pam, the super space spies. This section is based on the UK literacy programme, Letters and Sounds. Play games and watch songs and stories.' );
+INSERT INTO CATEGORIES(CAT_NAME,CAT_IMG,CAT_CONTENT) VALUES ('gramma & vocabulary','Banner/gramma_vocabulary.jpg', 'Do you want to practise your English grammar and learn new words? In this section you can learn about grammar rules, play word games and watch fun videos. Watch the grammar videos, play the grammar games and print the grammar worksheets.');
 
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Gramma/Gramma1.jpg','','',3);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Gramma/Gramma2.jpg','','',3);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Gramma/Gramma3.jpg','','',3);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon1.jpg','Listen to a song about a crazy house which is full of animals.','Video/animal_house.mp4',1);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon2.jpg','','Video/Baby_Shark.mp4',1);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon3.jpg','','Video/The_alphabet_song.mp4',1);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon4.jpg','','Video/Wheels_on_the_Bus .mp4',1);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read1.jpg','','Read/Reading1.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read2.jpg','','Read/Reading2.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read3.jpg','','Read/Reading3.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read4.jpg','','Read/Reading4.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Write/Write1.jpg','','Write/Writing1.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Write/Write2.jpg','','Write/Writing2.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Write/Write3.jpg','','Write/Writing3.png',2);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Vocabulary/Vocab1.jpg','Kids vocabulary - Sea Animals - Learn English for kids - English educational video','Video/VocabVid1.mp4',3);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Vocabulary/Vocab2.jpg','Kids vocabulary - Baby Animals - Learn English for kids - English educational video','Video/VocabVid2.mp4',3);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('','','Video/SpeakVid1.mp4',4);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('','','Video/SpeakVid2.mp4',4);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('','','Video/SpellVid1.mp4',4);
-INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('','','Video/SpellVid2.mp4',4);
+
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Gramma/Gramma1.jpg','','',4);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Gramma/Gramma2.jpg','','',4);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Gramma/Gramma3.jpg','','',4);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon1.jpg','Animal House','Video/animal_house.mp4',1);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon2.jpg','Baby Shark','Video/Baby_Shark.mp4',1);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon3.jpg','The alphabet song','Video/The_alphabet_song.mp4',1);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Listen/ListenIcon4.jpg','Wheels on the bus','Video/Wheels_on_the_Bus .mp4',1);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read1.jpg','Helping others','Read/Reading1.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read2.jpg','Say no to bullying','Read/Reading2.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read3.jpg','Superheroes','Read/Reading3.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Read/Read4.jpg','Seasons','Read/Reading4.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Write/Write1.jpg','Problem page','Write/Writing1.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Write/Write2.jpg','Penpal letter','Write/Writing2.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Write/Write3.jpg','Book review','Write/Writing3.png',2);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Vocabulary/Vocab1.jpg','Sea Animals','Video/VocabVid1.mp4',4);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Vocabulary/Vocab2.jpg','Baby Animals','Video/VocabVid2.mp4',4);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Banner/speak_spell.jpg','The television','Video/SpeakVid1.mp4',3);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Banner/speak_spell.jpg','The camping adventure','Video/SpeakVid2.mp4',3);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Banner/speak_spell.jpg','The spelling sports day','Video/SpellVid1.mp4',3);
+INSERT INTO LESSON(INTRO_IMG,LESSON_CONTENT,LINK,CAT_ID) VALUES('Banner/speak_spell.jpg','The boat adventure','Video/SpellVid2.mp4',3);
 
 
